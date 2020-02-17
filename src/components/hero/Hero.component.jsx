45b1b6150b1c29785cbf4037938122f68842f415
runandrerun <the.asantiagojr@gmail.com>
2020-02-17 16:18:43 -0500

    Hero Splash animated 'Learn more' button completed.

--- src/components/hero/Hero.component.jsx
+++ src/components/hero/Hero.component.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { ReactComponent as PawticularsLogo } from '../../_assets/img/pawticulars-paw.svg';
 import './Hero.styles.scss';
 
 const Hero = () => {
   return (
     <div className="hero">
+      <div className="logo-container">
+        <PawticularsLogo className="logo"/>
+      </div>
       <div className="hero-heading-container">
         <div className="hero-heading">
           <div className="hero-heading-primary">
@@ -12,6 +16,7 @@ const Hero = () => {
           <div className="hero-heading-sub">
             fosters to the rescue
           </div>
+          <a href="#" className="btn btn-white btn-animated">Learn more</a>
         </div>
       </div>
     </div>
