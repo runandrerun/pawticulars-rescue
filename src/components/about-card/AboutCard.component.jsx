8b8cc40980ca75eb18c001ffdf27e0c09ad457ee
runandrerun <the.asantiagojr@gmail.com>
2020-02-23 11:30:53 -0500

    Custom grid built. About section component started.

--- src/components/about-card/AboutCard.component.jsx
+++ src/components/about-card/AboutCard.component.jsx
@@ -3,64 +3,11 @@ import './AboutCard.styles.scss';
 
 const AboutCard = () => {
   return (
-    <section className="grid">
-      <div className="row">
-        <div className="col-1-of-2">
-          col 1
-        </div>
-        <div className="col-1-of-2">
-          col 2
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="col-1-of-3">
-          col 1 of 3
-        </div>
-        <div className="col-1-of-3">
-          col 2
-        </div>
-        <div className="col-1-of-3">
-          col 3
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="col-1-of-3">
-          col 1 of 3
-        </div>
-        <div className="col-2-of-3">
-          col 2
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="col-1-of-4">
-          col 1
-        </div>
-        <div className="col-1-of-4">
-          col 2
-        </div>
-        <div className="col-1-of-4">
-          col 3
-        </div>
-        <div className="col-1-of-4">
-          col 4
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="col-1-of-4">
-          col 1
-        </div>
-        <div className="col-1-of-4">
-          col 2
-        </div>
-        <div className="col-2-of-4">
-          col 3
-        </div>
-      </div>
-    </section>
+    <div className="about">
+      <h2 className="heading-secondary">
+        Finding Rescues their Forever Home
+      </h2>
+    </div>
   );
 };
 
